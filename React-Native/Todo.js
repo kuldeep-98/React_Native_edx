6247ec9fa996d94dac7e54fc119fcb5e0232c8fa
Kuldeep <kuldeepbhama4455@gmail.com>
2020-05-03 00:06:59 +0530

    add styles

--- React-Native/Todo.js
+++ React-Native/Todo.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {View, Button, Text, ScrollView, StyleSheet} from 'react-native'
+import {View, Button, Text, ScrollView, StyleSheet, Switch} from 'react-native'
 import  Constants   from 'expo-constants'
 
 let id = 0;
@@ -12,9 +12,13 @@ const styles = StyleSheet.create({
   appContainer:{
     paddingTop: Constants.statusBarHeight
   },
+  fill:{
+    flex: 1
+  },
 });
 const Todo = props => (
   <View style={styles.todoContainer}>
+    <Switch value={props.todo.checked} onValueChange={props.check}/>
     <Text>{props.todo.text}</Text>
     <Button onPress={props.removeTodo} title="delete" />
   </View>
@@ -53,14 +57,14 @@ export default class App extends React.Component {
   }
   render() {
     return (
-      <View style={styles.appContainer}>
+      <View style={[styles.appContainer, styles.fill]}>
         <Text>Todo count: {this.state.todos.length}</Text>
         <Text>
           Unchecked todo count:{" "}
           {this.state.todos.filter(todo => !todo.checked).length}
         </Text>
         <Button onPress={() => this.addTodo()} title="Add TODO" />
-        <ScrollView className="ul-style">
+        <ScrollView style={styles.fill}>
           {this.state.todos.map(todo => (
             <Todo
               todo={todo}
