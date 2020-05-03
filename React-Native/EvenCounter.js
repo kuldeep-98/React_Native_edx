196dd0acfa2a1d79ceed7ffe23d992b3f82400f9
Kuldeep <kuldeepbhama4455@gmail.com>
2020-05-03 23:11:28 +0530

    Show Counter on EvenNumber

---
+++ React-Native/EvenCounter.js
@@ -0,0 +1,53 @@
+import React from "react"
+import {View, Button, Text, ScrollView, StyleSheet, Switch} from 'react-native'
+import  Constants   from 'expo-constants'
+
+const styles = StyleSheet.create({
+  appContainer:{
+    flex:1,
+    alignItems:"center",
+    justifyContent:"center",
+  },
+  count:{
+    fontSize:50,
+  }
+});
+
+class CountEvenNumbers extends React.Component
+{
+  shouldComponentUpdate(nextProps){
+    return !(nextProps.count%2)
+  }
+  render(){
+    return(
+      <Text style={styles.count}>{this.props.count}</Text>
+    )
+  }
+}
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count:0,
+    }
+  }
+
+  componentDidMount(){
+    setInterval(this.inc,1000)
+  }
+
+  inc = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 1
+    }))
+  }
+  
+  render() {
+    return (
+      <View style={styles.appContainer}>
+       <CountEvenNumbers count={this.state.count} />
+      </View>
+    );
+  }
+}
