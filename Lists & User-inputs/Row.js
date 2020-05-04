2bbf797f460ef496404ba1d21a26736fd081f9f1
Kuldeep <kuldeepbhama4455@gmail.com>
2020-05-04 16:55:13 +0530

    Add Styles to contact

--- Lists & User-inputs/Row.js
+++ Lists & User-inputs/Row.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
+const styles = StyleSheet.create({
+  row:{padding:20},
+})
+
 const Row = ({ name, phone }) => (
-  <View>
+  <View style={styles.row} >
     <Text>{name}</Text>
     <Text>{phone}</Text>
   </View>
