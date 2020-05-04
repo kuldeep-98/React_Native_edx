9e0197116b2c6879b634a05049eea2d429eb7eab
Kuldeep <kuldeepbhama4455@gmail.com>
2020-05-04 16:54:47 +0530

    Adding FlatList

--- Lists & User-inputs/App.js
+++ Lists & User-inputs/App.js
@@ -1,33 +1,43 @@
-import React from 'react'
-import { Button, View, StyleSheet } from 'react-native'
-
+import React from 'react';
+import { Button, View, StyleSheet, ScrollView, Text,FlatList } from 'react-native';
+import Constants from 'expo-constants';
+import contacts from './Contacts';
+import Row from './Row'
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        paddingTop: Constants.statusBarHeight,
-    },
-})
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: Constants.statusBarHeight,
+  },
+});
 
-export class App extends React.Component {
-    state = {
-        showContacts: false
-    }
+export default class App extends React.Component {
+  state = {
+    showContacts: false,
+  };
 
-    toggleContact = () => {
-        this.setState(prevState =>
-            ({
-                showContacts: !prevState.showContacts
-            })
-        )
-    }
+  toggleContacts = () => {
+    this.setState(prevState => ({
+      showContacts: !prevState.showContacts,
+    }));
+  };
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <Button onPress={this.toggleContact} title="toggle" />
-            </View>
-        )
-    }
+  renderItem = obj => <Row {...(obj.item)}/>
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button onPress={this.toggleContacts} title="toggle contacts" />
+        {this.state.showContacts && (<FlatList 
+          renderItem = {obj =>  <Row {...(obj.item)}/>}
+          data = {contacts}
+        />)}
+        {/*<ScrollView>
+          {this.state.showContacts &&
+            contacts.map(contact => <Row {...contact} />)}
+        </ScrollView>*/}
+      </View>
+    );
+  }
 }
 
